c8e3e75ad1917394dd29abdffb2ecd99ad2f10b7
Humberto <humberto.robles.diez@gmail.com>
2023-01-26 08:42:10 +0100

    Added project

--- src/assets/projectsImages.js
+++ src/assets/projectsImages.js
@@ -1,23 +1,25 @@
-import library from './library.png'
-import dogger from './dogger.png'
-import medical from './medical-house.png'
-import nicePictures from './nice-pictures.png'
-import recipes from './recipes.png'
-import shopping from './shopping.png'
-import sprystore from './sprystore.png'
-import expenses from './expenses.png'
-import portfolio from './portfolio.png'
+import library from "./library.png";
+import dogger from "./dogger.png";
+import medical from "./medical-house.png";
+import nicePictures from "./nice-pictures.png";
+import recipes from "./recipes.png";
+import shopping from "./shopping.png";
+import sprystore from "./sprystore.png";
+import expenses from "./expenses.png";
+import portfolio from "./portfolio.png";
+import gutblocks from "./gut-blocks.png";
 
 const projectsImages = {
-  library,
-  dogger,
-  medical,
-  nicePictures,
-  recipes,
-  shopping,
-  sprystore,
-  expenses,
-  portfolio
-}
+    library,
+    dogger,
+    medical,
+    nicePictures,
+    recipes,
+    shopping,
+    sprystore,
+    expenses,
+    portfolio,
+    gutblocks,
+};
 
-export default projectsImages
+export default projectsImages;
--- src/components/Projects/CategoriesButtons.js
+++ src/components/Projects/CategoriesButtons.js
@@ -1,26 +1,35 @@
+const CategoriesButtons = ({ onclick, isActive }) => {
+    const categories = [
+        { id: "1", name: "All" },
+        { id: "2", name: "React JS" },
+        { id: "3", name: "Javascript" },
+        { id: "4", name: "API" },
+        { id: "5", name: "Ecommerce" },
+        { id: "6", name: "Wordpress" },
+        { id: "7", name: "php" },
+        { id: "8", name: "Plugins" },
+    ];
 
-const CategoriesButtons = ({onclick, isActive}) => {
+    return (
+        <ul className="flex flex-wrap items-center justify-center">
+            {categories.map(({ id, name }) => {
+                return (
+                    <li
+                        className={`${
+                            isActive === id
+                                ? "bg-primary"
+                                : "bg-[rgba(0,0,0,0.05)]"
+                        } rounded-md px-4 py-2 mb-8 mr-4  hover:bg-primary hover:text-white  transition-all duration-500 ease-[cubic-bezier(0.05,0,0,1)]`}
+                        onClick={onclick}
+                        key={id}
+                        data-id={id}
+                    >
+                        {name}
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
 
-  const categories = [
-    {id:'1', name:'All'},
-    {id:'2', name:'React JS'},
-    {id:'3', name:'Javascript'},
-    {id:'4', name:'API'},
-    {id:'5', name:'Ecommerce'},
-    {id:'6', name:'Wordpress'},
-    {id:'7', name:'php'},
-  ]
-
-  return (
-    <ul className='flex flex-wrap items-center justify-center'>
-      {categories.map(({ id,name} ) => {
-        return (
-
-          <li className={`${isActive === id ? 'bg-primary' : 'bg-[rgba(0,0,0,0.05)]'} rounded-md px-4 py-2 mb-8 mr-4  hover:bg-primary hover:text-white  transition-all duration-500 ease-[cubic-bezier(0.05,0,0,1)]`} onClick={onclick} key={id}  data-id={id} >{name}</li>
-        )
-      })}
-    </ul>
-  )
-}
-
-export default CategoriesButtons
+export default CategoriesButtons;
