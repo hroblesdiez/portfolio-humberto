78436e5732c3df9e605112eac95ff9ef5e737b19
Humberto <humberto.robles.diez@gmail.com>
2023-02-20 11:02:15 +0100

    Updating Projects

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,27 +1,32 @@
-import Logo from "../Navbar/Logo"
-import { GiSwordman } from 'react-icons/gi'
-import SectionTitle from "../General/SectionTitle"
-
+import Logo from "../Navbar/Logo";
+import { GiSwordman } from "react-icons/gi";
+import SectionTitle from "../General/SectionTitle";
 
 const About = () => {
   return (
-    <div className='w-full' name='about'>
+    <div className="w-full" name="about">
       <div className="px-4 md:px-20 lg:px-36 md:mt-0 mb-8 md:mb-12 lg:mb-20">
-       <SectionTitle icon={<GiSwordman size="28" />} text='About Me' />
-        <div className='flex flex-col justify-between items-center md:flex-row md:justify-between md:items-center'>
-          <div className='w-full h-full md:w-[35%] mb-8 md:mb-0 md:pr-8'>
+        <SectionTitle icon={<GiSwordman size="28" />} text="About Me" />
+        <div className="flex flex-col justify-between items-center md:flex-row md:justify-between md:items-center">
+          <div className="w-full h-full md:w-[35%] mb-8 md:mb-0 md:pr-8">
             <Logo classname="w-[150px] h-[150px] mx-auto" />
           </div>
-          <div className='w-full h-full md:w-[65%] '>
-            <h3 className='text-center text-lg md:text-2xl md:text-left text-title font-semibold mb-4'>I am a junior self-taught Front End Developer who is passionate about his work.</h3>
-            <p className='text-center text-base md:text-left text-paragraph'>I enjoy creating web pages and user interfaces.
-            I interact well with other people, I am continuosly looking for solutions to problems,learning new technologies and willing to keep improving.
-            My next goal is learn Redux and Node.js</p>
+          <div className="w-full h-full md:w-[65%] ">
+            <h3 className="text-center text-lg md:text-2xl md:text-left text-title font-semibold mb-4">
+              I am a junior self-taught Front End Developer who is passionate
+              about his work.
+            </h3>
+            <p className="text-center text-base md:text-left text-paragraph">
+              I enjoy creating web pages and user interfaces. I interact well
+              with other people, I am continuosly looking for solutions to
+              problems,learning new technologies and willing to keep improving.
+              Nowadays improving my php and php framework skills.
+            </p>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,54 +1,105 @@
-import { MdContactMail, MdEmail, MdPhone } from 'react-icons/md'
-import { FaTelegram } from 'react-icons/fa'
-import { IoLogoWhatsapp } from 'react-icons/io'
-import SectionTitle from '../General/SectionTitle'
-import ContactInfo from './ContactInfo'
-import ContactFormInput from './ContactFormInput'
-import Button from '../General/Button';
-import ContactSuccess from './ContactSuccess'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { MdContactMail, MdEmail, MdPhone } from "react-icons/md";
+import { FaTelegram } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+import SectionTitle from "../General/SectionTitle";
+import ContactInfo from "./ContactInfo";
+import ContactFormInput from "./ContactFormInput";
+import Button from "../General/Button";
+import ContactSuccess from "./ContactSuccess";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
-  const[isSubmitted, setIsSubmitted] = useState(false)
-  const navigate = useNavigate();
+  // const [isSubmitted, setIsSubmitted] = useState(false);
+  // const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setIsSubmitted(true)
-    navigate("/")
-    const inputsForm = document.querySelectorAll('form input,textarea')
-    inputsForm.forEach(input => input.value = '')
-  }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setIsSubmitted(true);
+  //   navigate("/");
+  //   const inputsForm = document.querySelectorAll("form input,textarea");
+  //   inputsForm.forEach((input) => (input.value = ""));
+  // };
 
   return (
-    <div className='w-full px-8' name='contact'>
-       <div className="px-4 md:px-10 lg:px-36 py-8 md:mt-0 mb-8 md:mb-12 lg:mb-20 max-w-[900px] mx-auto bg-white rounded-lg">
-        <SectionTitle icon={<MdContactMail size="28" />} text='Contact' />
-          <div className='grid grid-cols-1 gap-5 md:grid-cols-2 px-4 sm:px-0'>
-              <div className='flex flex-col items-start justify-start space-y-4'>
-                  <ContactInfo icon={<MdEmail size="22" />} title="Email" text="humberto.robles.diez@gmail.com" />
-                  <ContactInfo icon={<MdPhone size="22" />} title="Phone" text="+48 690 620 188" />
-                  <ContactInfo icon={<FaTelegram size="22" />} title="Telegram" text="+48 690 620 188" />
-                  <ContactInfo icon={<IoLogoWhatsapp size="22" />} title="Whatsapp" text="+48 690 620 188" />
-              </div>
-              <div className='w-full'>
-                <form name="contact" action="/contact/success" method="POST" onSubmit={handleSubmit}>
-                  <input type="hidden" name="form-name" value="contact" />
-                  <ContactFormInput element= 'input' labelFor="name" label="Name" type="text" name="name" placeholder="Your name" required={true} />
-                  <ContactFormInput element= 'input' labelFor="email" label="Email" type="email" name="email" placeholder="Your email" required={true} />
-                  <ContactFormInput element= 'input' labelFor="subjet" label="Subject" type="text" name="subject" placeholder="Subject" />
-                  <ContactFormInput element= 'textarea' labelFor="message" label="Message" name="message" placeholder="Your message"
-                  rows="5" required={true} />
-                  <div data-netlify-recaptcha="true"></div>
-                  <Button type="submit" text="Submit"/>
-                </form>
-                {isSubmitted && (<ContactSuccess />)}
-              </div>
+    <div className="w-full px-8" name="contact">
+      <div className="px-4 md:px-10 lg:px-36 py-8 md:mt-0 mb-8 md:mb-12 lg:mb-20 max-w-[900px] mx-auto bg-white rounded-lg">
+        <SectionTitle icon={<MdContactMail size="28" />} text="Contact" />
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 px-4 sm:px-0">
+          <div className="flex flex-col items-start justify-start space-y-4">
+            <ContactInfo
+              icon={<MdEmail size="22" />}
+              title="Email"
+              text="humberto.robles.diez@gmail.com"
+            />
+            <ContactInfo
+              icon={<MdPhone size="22" />}
+              title="Phone"
+              text="+48 690 620 188"
+            />
+            <ContactInfo
+              icon={<FaTelegram size="22" />}
+              title="Telegram"
+              text="+48 690 620 188"
+            />
+            <ContactInfo
+              icon={<IoLogoWhatsapp size="22" />}
+              title="Whatsapp"
+              text="+48 690 620 188"
+            />
           </div>
-       </div>
+          <div className="w-full">
+            <form
+              name="contact"
+              //action="/contact/success"
+              method="POST"
+              //onSubmit={handleSubmit}
+            >
+              <input type="hidden" name="form-name" value="contact" />
+              <ContactFormInput
+                element="input"
+                labelFor="name"
+                label="Name"
+                type="text"
+                name="name"
+                placeholder="Your name"
+                required={true}
+              />
+              <ContactFormInput
+                element="input"
+                labelFor="email"
+                label="Email"
+                type="email"
+                name="email"
+                placeholder="Your email"
+                required={true}
+              />
+              <ContactFormInput
+                element="input"
+                labelFor="subjet"
+                label="Subject"
+                type="text"
+                name="subject"
+                placeholder="Subject"
+              />
+              <ContactFormInput
+                element="textarea"
+                labelFor="message"
+                label="Message"
+                name="message"
+                placeholder="Your message"
+                rows="5"
+                required={true}
+              />
+              <div data-netlify-recaptcha="true"></div>
+              <Button type="submit" text="Submit" />
+            </form>
+            {/* {isSubmitted && <ContactSuccess />} */}
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
